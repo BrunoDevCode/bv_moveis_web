ec023124c897e437ec7e51d731525c80650bee42
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-30 12:57:48 -0300

    Adding page to sing in user

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,68 @@
+import { FormEvent, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+
+import styles from '../../styles/login.module.css';
+
 const Login: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { push } = useRouter();
+
+  async function handleMakeLogin(e: FormEvent) {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    try {
+      const { data: { token } } = await api.post('/admin/login', data);
+
+      localStorage.setItem('token', token);
+
+      push('/admin/launchpage');
+    } catch (error) {
+      alert('Erro no login, por favor tente novamente');
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logoBV}>
+          <img src="/logo.jpg" alt="Logo BV Moveis" />
+        </div>
+        <h1>BV Móveis</h1>
+      </header>
+
+      <div className={styles.container}>
+        <form onSubmit={handleMakeLogin} autoComplete='off'>
+          <h2>Bem-vindo de volta</h2>
+
+          <div className={styles.input_group}>
+            <label htmlFor="email">E-mail :</label>
+            <input type="email" id='email' value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+
+          <div className={styles.input_group}>
+            <label htmlFor="pass">Senha :</label>
+            <input type="password" id='pass' value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+
+          <button type='submit'>Login</button>
+
+          <Link href="/">
+            <a className={styles.return_link}>Cancelar e Ir para homepage</a>
+          </Link>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
