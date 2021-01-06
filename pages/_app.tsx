2d5311afd2c2adf301a9514f76015879f7ecb0ae
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-06 00:19:29 -0300

    Adding SEO Strategys

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>BV Móveis Rusticos</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta name='keywords' content='BV Móveis, Móveis Rústicos, Mesas, Bancos, Armários, Madeira' />
+        <meta name='description' content='Venha conferir nossos móveis feitos de madeira rústica. Avaliamos seu projeto!!' />
+        <meta name='author' content='https://github.com/BrunoDevCode' />
+        <meta httpEquiv="content-language" content="pt-br" />
       </Head>
       <Component {...pageProps} />
     </>
