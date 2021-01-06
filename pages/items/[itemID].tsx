18875d183c4826c7fe090f866bc8c74e5b4a14f6
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-06 00:24:57 -0300

    Adding a single item page

--- pages/items/[itemID].tsx
+++ pages/items/[itemID].tsx
@@ -1,63 +1,96 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import Head from 'next/head';
 import { api } from '../../services/api';
-import { Item } from '../../components/Item';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 import styles from '../../styles/item.module.css';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { FaWhatsapp } from 'react-icons/fa';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 const ItemPage: React.FC = ({ item }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  let currentImageIndex = 0;
 
   useEffect(() => {
-    // const images = document.querySelectorAll('#slider img'), max = images.length;
+    const images = document.querySelectorAll('#slider img');
 
-    // console.log(images)
-  })
+    console.log(images);
 
-  console.log(item);
-
+    images[0].setAttribute('id', 'selected');
+  });
 
   function handleLeftImage() {
-    console.log(item.images);
+    const images = document.querySelectorAll('#slider img'), max = images.length;
+
+    images[currentImageIndex].removeAttribute('id');
+
+    currentImageIndex--;
+
+    if (currentImageIndex < 0)
+      currentImageIndex = max - 1;
+
+    console.log(currentImageIndex)
+
+    images[currentImageIndex].setAttribute('id', 'selected');
   }
 
   function handleRightImage() {
+    const images = document.querySelectorAll('#slider img'), max = images.length;
 
+    images[currentImageIndex].removeAttribute('id');
+
+    currentImageIndex++;
+
+    if (currentImageIndex >= max)
+      currentImageIndex = 0;
+
+    console.log(currentImageIndex)
+
+    images[currentImageIndex].setAttribute('id', 'selected');
   }
 
   return (
     <>
+      <Head>
+        <title>{item.title} - BV Móveis</title>
+        <meta name='description' content={`${item.description} - BV Móveis`} />
+      </Head>
+
       <Header />
-      <div className={styles.banner_container}>
-        <div id='slider' className={styles.slider}>
-          <button onClick={handleLeftImage}>
-            <FiChevronLeft size={24} color='white' />
-          </button>
 
-          {item.images.map((image) => {
-            return (
-              <img key={image._id} src={image.url} />
-            );
-          })}
+      <main className={styles.container}>
+        <div className={styles.banner_container}>
+          <div id='slider' className={styles.slider}>
+            {item.images.map((image) => {
+              return (
+                <img key={image._id} src={image.url} />
+              );
+            })}
+          </div>
 
-          <button onClick={handleRightImage}>
-            <FiChevronRight size={24} color='white' />
+          <div className={styles.button_group}>
+            <button onClick={handleLeftImage}>
+              <FiChevronLeft size={28} color='green' />
+            </button>
+
+            <button onClick={handleRightImage}>
+              <FiChevronRight size={28} color='green' />
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.description}>
+          <h2>{item.title}</h2>
+          <p className={styles.describe}>{item.description}</p>
+
+          <button className={styles.whats}>
+            <FaWhatsapp size={24} color='green' />
+            <p>Faça um orçamento</p>
           </button>
         </div>
-      </div>
-      {/* <h1>{item.title}</h1>
-      <p>{item.description}</p> */}
+      </main>
 
-      <p>Você tem algum projeto diferente, podemos dar uma olhada</p>
-
-      <button>
-        <FaWhatsapp size={24} color='white' />
-        <p>Mande uma mensagem</p>
-      </button>
       <Footer />
     </>
   );
