ee2c248fb9286c4177726d8266098f7551106882
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-09 20:47:13 -0300

    Adding Forms to manage page

--- pages/admin/launchpage.tsx
+++ pages/admin/launchpage.tsx
@@ -8,7 +8,7 @@ import styles from '../../styles/launchpage.module.css';
 const Launchpage: React.FC = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [isAvailable, setIsAvailable] = useState(false);
+  const [isAvailable, setIsAvailable] = useState(true);
   const [isHomepage, setIsHomepage] = useState(false);
 
   const { push } = useRouter();
@@ -40,46 +40,63 @@ const Launchpage: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.list_item_list}>
+      <details className={styles.list_item_list}>
+        <summary>Listar dos produtos</summary>
+      </details>
 
-      </div>
+      <details className={styles.new_item_container}>
+        <summary>Criar Novo Produto</summary>
 
-      <div className={styles.new_item_container}>
-        <form>
-          <h2>Criar Item</h2>
-
+        <form className={styles.form_new}>
           <div className={styles.input_group}>
             <label htmlFor="title">Título :</label>
             <input type="text" id='title' value={title} onChange={e => setTitle(e.target.value)} />
           </div>
 
           <div className={styles.input_group}>
-            <label htmlFor="description">Descrição :</label>
+            <label htmlFor="description">Descrição do produto :</label>
             <textarea id="description" value={description} onChange={e => setDescription(e.target.value)}></textarea>
           </div>
 
-          <div className={styles.input_group}>
-            <label htmlFor="isHomepage">Aparecerá na homepage</label>
-            <Switch
-              checked={isHomepage}
-              onChange={handleToogleIsHomepage}
-            />
+          <div className={styles.isHomepage_container}>
+            <div className={styles.isHomepage}>
+              <label htmlFor="isHomepage">Ficara vísivel na pagina Inicial ?</label>
+              <Switch
+                checked={isHomepage}
+                onChange={handleToogleIsHomepage}
+                checkedIcon={false}
+                uncheckedIcon={false}
+                height={20}
+                width={40}
+                handleDiameter={20}
+                onColor='#4C191B'
+              />
+            </div>
+
+            <span>Lembrando que se ja houver 6 produtos, pode ser que este não apareça</span>
           </div>
 
-          <div className={styles.input_group}>
-            <label htmlFor="isAvailable">Aparecerá na isAvailable</label>
+          <div className={styles.isAvailable}>
+            <label htmlFor="isAvailable">Há este produto no estoque ?</label>
             <Switch
               checked={isAvailable}
               onChange={handleToogleIsAvailable}
+              checkedIcon={false}
+              uncheckedIcon={false}
+              height={20}
+              width={40}
+              handleDiameter={20}
+              onColor='#4C191B'
             />
           </div>
 
-          <button onClick={handleCreateItem}>Criar Produto</button>
+          <button onClick={handleCreateItem} className={styles.submit_form}>Criar Produto</button>
         </form>
-      </div>
 
-      <div className={styles.update_item_container}>
-      </div>
+        <form className={styles.form_image_up}>
+
+        </form>
+      </details>
 
       <div className={styles.delete_item_container}>
 
