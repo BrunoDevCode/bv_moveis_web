afce06a7deb5fe9420c192bcc2cb2957aaafde8c
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-25 23:21:47 -0300

    Adding Content Security Policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>BV Móveis Rusticos</title>
-        <link rel='icon' href='/favicon.ico' />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        <meta charSet='utf-8' />
+        <meta name='robots' content='index, follow' />
         <meta name='keywords' content='BV Móveis, Móveis Rústicos, Mesas, Bancos, Armários, Madeira' />
         <meta name='description' content='Venha conferir nossos móveis feitos de madeira rústica. Avaliamos seu projeto!!' />
         <meta name='author' content='https://github.com/BrunoDevCode' />
         <meta httpEquiv="content-language" content="pt-br" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <Component {...pageProps} />
     </>
