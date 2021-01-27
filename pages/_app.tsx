a335e93ba12cebd8bd22d4924a791372be562392
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-27 01:01:48 -0300

    Remove meta author

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name='robots' content='index, follow' />
         <meta name='keywords' content='BV Móveis, Móveis Rústicos, Mesas, Bancos, Armários, Madeira' />
         <meta name='description' content='Venha conferir nossos móveis feitos de madeira rústica. Avaliamos seu projeto!!' />
-        <meta name='author' content='https://github.com/BrunoDevCode' />
         <meta httpEquiv="content-language" content="pt-br" />
         <link rel='icon' href='/favicon.ico' />
       </Head>
