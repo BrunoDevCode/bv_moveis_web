308e652b18fd8b58a00f6a914ed46409e8446efb
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-04-03 00:02:54 -0300

    feat: adding footer and GlobalStyle of styled-c

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/global';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>BV Móveis Rústicos</title>
+      </Head>
+
+      <Component {...pageProps} />
+
+      {/* Diminuir font do footer  */}
+      <footer>
+        <p>
+          Todos os direitos reservados &copy; Copyright{' '}
+          {new Date().getFullYear()}
+        </p>
+      </footer>
+
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default MyApp;
